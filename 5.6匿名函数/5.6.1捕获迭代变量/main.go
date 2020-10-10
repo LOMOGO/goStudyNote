12e2cce2133811b14a33e5e430eaa53ce599764f
LOMOGO <46417831+LOMOGO@users.noreply.github.com>
2020-10-10 20:12:14 +0800

    Add files via upload

---
+++ 5.6匿名函数/5.6.1捕获迭代变量/main.go
@@ -0,0 +1,33 @@
+//如果你的代码的循环结构中使用了函数值，且函数值需要使用循环变量的话，那么你就需要注意了
+package main
+
+import "fmt"
+
+/*
+var rmdirs []func()
+//处理这类问题的时候你需要引入一个局部变量并将循环变量的值赋值给局部变量，然后才能在函数值中使用局部变量，如果不进行赋值操作的话，那么函数值操作的值都是循环变量最后一次迭代的值
+for _, d := range tempDirs() {
+	dir := d
+	os.MkdirAll(dir, 0755)
+	rmdirs = append(rmdirs, func() {
+		os.RemoveAll(dir)
+	})
+}
+for _, rmdir := range rmdirs{
+	rmdir()
+}
+
+*/
+
+func main() {
+	slice_func := []func(){}
+	for i := 0; i < 5; i++ {
+		ni := i
+		slice_func = append(slice_func, func() {
+			fmt.Println(ni)
+		})
+	}
+	for _, v := range slice_func {
+		v()
+	}
+}
